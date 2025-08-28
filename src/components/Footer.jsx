f9e6471c3927c0241ef0aa0211de0095264c4a70
trivikramm <trivikram100699@gmail.com>
2025-08-28 09:30:35 +0530

    deleted direct whatsappabcdefg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <footer id="contact" className="footer-section">
       <div className="contact-info">
-        <strong>Contact:</strong> +91-8179048058 (WhatsApp).<br/>
+        {/*<strong>Contact:</strong> +91- (WhatsApp).<br/>*/}
         Join our community channels for updates & opportunities.
       </div>
       <div className="social-links">
         <a href="https://t.me/FreelanceJobs0" aria-label="Telegram Channel" target="_blank" rel="noopener noreferrer"><FaTelegramPlane /></a>
-        <a href="https://wa.me/918179048058" aria-label="Direct WhatsApp Chat" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
+        <a href="https://chat.whatsapp.com/KbtOV77EiOVG4gKYWafkfs?mode=ems_share_c" aria-label="Direct WhatsApp Chat" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
         {/* <a href="https://www.linkedin.com/in/freelancejobs0" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a> */}
       </div>
       <p className="copyright">&copy; {new Date().getFullYear()} Freelance Jobs. All rights reserved.</p>
